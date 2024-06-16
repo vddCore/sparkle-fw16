7e4420fb6cb48125e677f1b42d7a9223f75c068d
vddCore <vddcore@outlook.com>
2024-06-16 21:15:27 +0200

    maybe pack the structure actually

--- include/protocol/glitter.h
+++ include/protocol/glitter.h
@@ -13,7 +13,7 @@ typedef struct glitter_device_info
     uint8_t display_width;     /* 9 for the official LED matrix module */
     uint8_t display_height;    /* 34 for the official LED matrix module */
     int32_t timeout_ms;
-} glitter_device_info_t;
+} __attribute__((packed)) glitter_device_info_t;
 
 typedef enum glitter_basic_command
 {
